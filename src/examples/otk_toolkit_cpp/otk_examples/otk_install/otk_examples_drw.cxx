42b3f227aba2754662ead5625adb5b37117c9958
Nicogene <nicolo.genesio@iit.it>
2023-03-02 16:19:57 +0100

    examples: remove examples that does not work

--- src/examples/otk_toolkit_cpp/otk_examples/otk_install/otk_examples_drw.cxx
+++
@@ -1,5 +0,0 @@
-/*
-	Copyright (c) 2022 PTC Inc. and/or Its Subsidiary Companies. All Rights Reserved.
-*/
-
-#include "OTKXDrawing.cxx"
--- src/examples/otk_toolkit_cpp/otk_examples/otk_install/otk_examples_ui.cxx
+++
@@ -1,6 +0,0 @@
-/*
-	Copyright (c) 2022 PTC Inc. and/or Its Subsidiary Companies. All Rights Reserved.
-*/
-
-#include "OTKXUIFCDialog.cxx"
-#include "OTKXUIFCNakedWin.cxx"
